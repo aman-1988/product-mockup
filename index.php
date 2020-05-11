c97e2f35f9bad0ac659104e12590b54967600be4
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-05-11 22:14:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
-echo "New APP";
+//echo "New APP";
 $imglink = "https://eparcel15.herokuapp.com/1-29.png";
+
+
+
+    $callback ='mycallback';
+ 
+    if(isset($_GET['mycallback']))
+    {
+        $callback = $_GET['mycallback'];
+    }   
+    $arr =array();
+    $arr['name']="Ravishanker";
+    $arr['age']=32; 
+    $arr['location']="India";   
+ 
+    echo $callback.'(' . json_encode($arr) . ')';
+ 
+
 ?>
