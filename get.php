763868bedd3bc34f13bcd8f2321fbae07f944df8
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-05-11 23:45:58 +0530

    Create get.php

---
+++ get.php
@@ -0,0 +1,15 @@
+<?php
+header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+ 
+if(isset($HTTP_RAW_POST_DATA)) {
+  parse_str($HTTP_RAW_POST_DATA,$arr); 
+  $arr['extra']='1.POST Request from hayageek.com';
+  echo json_encode($arr);
+}
+else
+{
+    $_POST['extra']='2.POST Request from hayageek.com';
+    echo json_encode($_POST);
+}
+?>
