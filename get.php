b717ecec22aeee803f35b8ce2989f3958e821767
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-08-17 06:35:48 -0700

    Update get.php

--- get.php
+++ get.php
@@ -13,7 +13,10 @@ else
   //  echo json_encode($_POST);
  $name = $_POST['name'];
  $age = $_POST['age'];
+ if($age == "22222")
+ {
  echo "<img src='http://apitest.harjihousing.com/image-preview-upload/uploads/Mens-Summer-Fashion.png'>";
+ }
  //echo $name." - ".$age;
  //echo "aman";
 }
