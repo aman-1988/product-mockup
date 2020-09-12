ae334fa64358c716afeca199cfb853f03caf75b5
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-09-12 22:54:23 +0530

    Create get4.php

---
+++ get4.php
@@ -0,0 +1,31 @@
+<?php
+header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+ 
+if(isset($HTTP_RAW_POST_DATA)) {
+  parse_str($HTTP_RAW_POST_DATA,$arr); 
+  $arr['extra']='1.POST Request from hayageek.com';
+  echo json_encode($arr);
+}
+else
+{
+    $_POST['extra']='2.POST Request from hayageek.com';
+  //  echo json_encode($_POST);
+ $name = $_POST['name'];
+ $age = $_POST['age'];
+ if($age == "22222")
+ {
+ //echo '<img src="https://bouteeki-mockup.herokuapp.com/image.jpg" style="max-width:50%;">';
+ 
+ 
+ 
+ 
+ 
+ 
+ } else {
+ // echo "Code is not correct";
+ }
+ //echo $name." - ".$age;
+ //echo "aman";
+}
+?>
