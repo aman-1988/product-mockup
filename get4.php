dc49d9acb70ae73f2ab7a10064fe1a352888f7a8
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-09-14 03:31:06 -0700

    Update get4.php

--- get4.php
+++ get4.php
@@ -1,15 +1,38 @@
 <?php
 header('Access-Control-Allow-Origin: *'); 
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+
+$SHOPIFY_SHOP = 'goodnessforu.myshopify.com'; //For eg: storedenavin.myshopify.com
+
+function getorder($url)
+{
+$ch = curl_init($url);      
+//curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");                                                                     
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+    'Content-Type: application/json',  
+    'Authorization: Basic NmM1ZjE2OWRhOGRkMGI4MjdmNzk4OGM0MjQ5NGVhYzE6c2hwcGFfMDQ1YTA0ODdiNDlmODYyNTcxMjFiYmQzYTYxZjcxOWM=')                                                                     
+);                                                                                                                   
+$output = curl_exec($ch);
+curl_close($ch); 
+$json_data_shopify = json_decode($output,true);   
+return $json_data_shopify;
+}
+
+
+
  
 if(isset($HTTP_RAW_POST_DATA)) {
   parse_str($HTTP_RAW_POST_DATA,$arr); 
-  $arr['extra']='1.POST Request from hayageek.com';
+  $arr['extra']='1.POST Request from demo.com';
   echo json_encode($arr);
 }
 else
 {
-    $_POST['extra']='2.POST Request from hayageek.com';
+    $_POST['extra']='2.POST Request from demo.com';
   //  echo json_encode($_POST);
  $name = $_POST['name'];
  $age = $_POST['age'];
@@ -17,9 +40,11 @@ else
  {
  //echo '<img src="https://bouteeki-mockup.herokuapp.com/image.jpg" style="max-width:50%;">';
  
- echo '<a href="https://goodnessforu.myshopify.com/cart/32056468766783:1">Buy Now</a>';
+ //echo '<a href="https://goodnessforu.myshopify.com/cart/32056468766783:1">Buy Now</a>';
  
- 
+ $productss = getorder("https://".$SHOPIFY_SHOP."/admin/api/2020-07/orders/".$name.".json");
+echo "<pre>";
+print_r($productss);
  
  
  } else {
