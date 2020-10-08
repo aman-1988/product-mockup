5e7e1644fc339e72b650217f242c66cafc2dfeeb
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-10-08 03:36:11 -0700

    Update get4.php

--- get4.php
+++ get4.php
@@ -92,7 +92,7 @@ $checkout_country = $cust_country;
          //echo $protitle." - ".$variant_title;
          
          $productss = getorder("https://".$SHOPIFY_SHOP."/admin/api/2020-07/products/".$productid.".json");
-echo "<pre>";
+//echo "<pre>";
 print_r($productss['product']);
 //$product_line_items = $productss['products'];  
 $product_line_items = $productss['product'];  
