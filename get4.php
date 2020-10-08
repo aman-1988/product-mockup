6291ed75ccedd59cff58101434903c89f9772554
aman-1988 <65157882+aman-1988@users.noreply.github.com>
2020-10-08 03:28:55 -0700

    Update get4.php

--- get4.php
+++ get4.php
@@ -58,13 +58,23 @@ else
      $cust_phone = $orderdata['shipping_address']['phone'];
      
      
-     echo $custemail."<br>";
-     echo $cust_first_name." ".$cust_last_name."<br>";
-     echo $cust_address1."<br>";
-     echo $cust_city."<br>";
-     echo $cust_zip."<br>";
-     echo $cust_country."<br>";
+    // echo $custemail."<br>";
+    // echo $cust_first_name." ".$cust_last_name."<br>";
+     //echo $cust_address1."<br>";
+     //echo $cust_city."<br>";
+    // echo $cust_zip."<br>";
+    // echo $cust_country."<br>";
      
+     
+     
+     $checkout_email = $custemail;
+$checkout_city = $cust_city;
+$checkout_first_name = $cust_first_name;
+$checkout_last_name = $cust_last_name;
+$checkout_address1 = $cust_address1;
+$checkout_zip = $cust_zip;
+$checkout_country = $cust_country;
+//$checkout_province = "New York";
      
      
    $line_items = $orderss['order']['line_items'];  
@@ -77,9 +87,124 @@ else
          $protitle = $line_items[$keys1]['title'];
          $proname = $line_items[$keys1]['name'];
          $variant_title = $line_items[$keys1]['variant_title'];
-         echo $productid;
-         echo "<br>";
+        // echo $productid;
+        // echo "<br>";
          //echo $protitle." - ".$variant_title;
+         
+         $productss = getorder("https://".$SHOPIFY_SHOP."/admin/api/2020-07/products/".$productid.".json");
+//echo "<pre>";
+//print_r($productss['product']);
+//$product_line_items = $productss['products'];  
+$product_line_items = $productss['product'];  
+
+
+
+         $productid2 = $product_line_items['id'];
+         $productdesc = $product_line_items['body_html'];
+         //$variant_id2 = $product_line_items['variant_id'];
+         $protitle2 = $product_line_items['title'];
+         $proname2 = $product_line_items['name'];
+         $handle1 = $product_line_items['handle'];
+         $proimgs1 = $product_line_items['image']['src'];
+         $price1 = $product_line_items['variants'][0]['price'];
+         $defaultvar = $product_line_items['variants'][0]['id'];
+         $compare_at_price = $product_line_items['variants'][0]['compare_at_price'];
+         
+         $allvariants = $product_line_items['variants'];
+        // echo $protitle2;
+         
+         foreach ($allvariants as $keys4 => $values4)
+         {
+              $vartid = $allvariants[$keys4]['id'];
+              $product_id = $allvariants[$keys4]['product_id'];
+              $varttitle = $allvariants[$keys4]['title'];
+              $vartprice = $allvariants[$keys4]['price'];
+              $vartoption1 = $allvariants[$keys4]['option1'];
+              $vartoption2 = $allvariants[$keys4]['option2']; 
+              $vartoption3 = $allvariants[$keys4]['option3'];
+              $vartinventory_item_id = $allvariants[$keys4]['inventory_item_id'];
+              $vartqty = $allvariants[$keys4]['inventory_quantity'];
+              //echo $varttitle."<br>";
+             // $varnames[] = "<option value='".$vartid."'>".$varttitle."</option>";
+              echo '<input type="hidden" id="'.$vartid.'" value="'.$vartprice.'">';
+              //$varprices[] = $vartprice;
+          }
+
+?>
+
+<script>
+     function getvarients(var1)
+     {
+         
+     //alert(var1);
+     $('#selectedvar<?=$product_id;?>').val(var1);
+     var pricess = $('#' + var1).val();
+     $('#pricess<?=$product_id;?>').val(pricess);
+     $('#varprices<?=$product_id;?>').html('$ ' + pricess);
+     //alert($('#' + var1).val());
+     }
+     
+     function getsubmitted()
+     {
+     var selectedvar1 =  $('#selectedvar<?=$product_id;?>').val();    
+    //alert(selectedvar1);
+    var checkoutlink = "https://physix-gear-sport.myshopify.com/cart/" + selectedvar1 + ":1?checkout[email]=<?=$checkout_email;?>&checkout[shipping_address][city]=<?=$checkout_city;?>&checkout[shipping_address][first_name]=<?=$checkout_first_name;?>&checkout[shipping_address][last_name]=<?=$checkout_last_name;?>&checkout[shipping_address][address1]=<?=$checkout_address1;?>&checkout[shipping_address][zip]=<?=$checkout_zip;?>&checkout[shipping_address][country]=<?=$checkout_country;?>&checkout[shipping_address][province]=<?=$checkout_province;?>";
+    location.href = checkoutlink;
+     }
+     
+ </script>
+
+<div style="float:left; width:40%; border:1px solid #ccc; padding:15px;  margin:15px 0px;">
+<h1 style="float:left; width:100%; font-size:18px; color:#0d7700; margin-bottom:10px; margin-top:0px;"><?=$suo_popup_title;?></h1>
+<h4 style="float:left; width:100%; font-size:14px; color:#3a3a3a; margin-bottom:15px; margin-top:0px;"><?=$suo_popup_description;?></h4>
+<div style="float:left; width:100%;">
+<img src="<?=$proimgs1;?>" style="float:left; width:20%;">    
+<div style="float:left; width:54%; margin-left:5%;">
+<h3 style="float:left; width:100%; color:#045484; font-size:16px; margin-top:0px;"><?=$protitle2;?></h3> 
+<select name="allvarients" onchange="getvarients(this.value);">
+<?php 
+foreach ($allvariants as $keys4 => $values4)
+{
+    $vartid = $allvariants[$keys4]['id'];
+     $varttitle = $allvariants[$keys4]['title'];
+     echo  "<option value='".$vartid."'>".$varttitle."</option>";
+}
+
+?>
+</select>
+<input type="hidden" id="selectedvar<?=$product_id;?>" value="<?=$defaultvar;?>">
+<input type="hidden" id="pricess<?=$product_id;?>" value="<?=$price1;?>">
+</div>    
+ 
+<div style="float:left; width:15%; margin-left:5%;"> 
+<span style="font-size:18px; color:#9c0707; font-weight:bold; float:right;" id="varprices<?=$product_id;?>">$ <?=$price1;?></span>
+</div>
+
+        
+</div>
+
+<div style="float:left; width:100%; margin-top:2%;" ><button onclick="getsubmitted();" style="float:right; padding:7px 15px; font-size:15px; cursor:pointer; background:#00691c; color:#fff; outline:none; border:none; border-radius:5px;">Buy Now</button>
+</div> 
+
+</div>
+
+
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+ <?php        
+         
+         
      }
      
  
